9f1c74e35ba7474c91384cb1598f2544d87ed98f
swastikbanerjee <swastik.banerjee@msam.christuniversity.in>
2024-07-13 01:12:53 +0530

    imported initial dependencies, created chain

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.llms import Ollama
+from langchain_core.output_parsers import StrOutputParser
+
+prompt = ChatPromptTemplate.from_messages([("system",""),("user","")])
+llm = Ollama(model="llama3")
+op = StrOutputParser()
+
+chain = prompt | llm | op
+
